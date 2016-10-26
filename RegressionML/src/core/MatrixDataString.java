c50715302a9c4b3c918a6f0b0b2fd37bf7897f5e
Vibhor <vibhor1319@iiitd.ac.in>
2016-10-26 19:20:57 +0530

    logistic classification core matrix formulation

--- RegressionML/src/core/MatrixDataString.java
+++ RegressionML/src/core/MatrixDataString.java
@@ -3,40 +3,57 @@ package core;
 import java.util.HashMap;
 
 public class MatrixDataString {
-String [][] columnMatrix;
-public String[][] getColumnMatrix() {
-	return columnMatrix;
-}
-public void setColumnMatrix(String[][] columnMatrix) {
-	this.columnMatrix = columnMatrix;
-}
-public String[][] getMatrix() {
-	return matrix;
-}
-public void setMatrix(String[][] matrix) {
-	this.matrix = matrix;
-}
-public HashMap<String, String[]> getDynamic_matrix() {
-	return dynamic_matrix;
-}
-public void setDynamic_matrix(HashMap<String, String[]> dynamic_matrix) {
-	this.dynamic_matrix = dynamic_matrix;
-}
-public HashMap<String, String[]> getNormalized_dynamic_matrix() {
-	return normalized_dynamic_matrix;
-}
-public void setNormalized_dynamic_matrix(HashMap<String, String[]> normalized_dynamic_matrix) {
-	this.normalized_dynamic_matrix = normalized_dynamic_matrix;
-}
+
+
 public String[] getColumnHeaders() {
 	return columnHeaders;
 }
 public void setColumnHeaders(String[] columnHeaders) {
 	this.columnHeaders = columnHeaders;
 }
-String [][] matrix ;
-	HashMap<String,String[]> dynamic_matrix = new HashMap<String, String[]>();
-	HashMap<String,String[]> normalized_dynamic_matrix = new HashMap<String, String[]>();
-	//HashMap<String,Dou> normalized_norm_value = new HashMap<String, Double>();
+	
+HashMap<Integer, String> name = new HashMap<Integer, String>();
+public HashMap<Integer, String> getName() {
+	return name;
+}
+public void setName(HashMap<Integer, String> name) {
+	this.name = name;
+}
+public HashMap<Integer, String> getReview() {
+	return review;
+}
+public void setReview(HashMap<Integer, String> review) {
+	this.review = review;
+}
+public HashMap<Integer, Integer> getRating() {
+	return rating;
+}
+public void setRating(HashMap<Integer, Integer> rating) {
+	this.rating = rating;
+}
+public HashMap<Integer, String> getReviewClean() {
+	return reviewClean;
+}
+public void setReviewClean(HashMap<Integer, String> reviewClean) {
+	this.reviewClean = reviewClean;
+}
+public HashMap<Integer, Integer> getSentiment() {
+	return sentiment;
+}
+public void setSentiment(HashMap<Integer, Integer> sentiment) {
+	this.sentiment = sentiment;
+}
+public HashMap<Integer, HashMap<String, Integer>> getWordCount() {
+	return wordCount;
+}
+public void setWordCount(HashMap<Integer, HashMap<String, Integer>> wordCount) {
+	this.wordCount = wordCount;
+}
+
+HashMap<Integer, String> review = new HashMap<Integer,String>();
+HashMap<Integer, Integer> rating = new HashMap<Integer, Integer>();
+HashMap<Integer, String> reviewClean = new HashMap<Integer, String>();
+HashMap<Integer, Integer> sentiment = new HashMap<Integer, Integer>();
+HashMap<Integer, HashMap<String, Integer>> wordCount = new HashMap<Integer, HashMap<String, Integer>>();
 	String columnHeaders[];
 }
--- RegressionML/src/core/MatrixFormulation.java
+++ RegressionML/src/core/MatrixFormulation.java
@@ -5,7 +5,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.Iterator;
+import java.util.HashSet;
 import java.util.LinkedList;
 
 import com.google.gson.Gson;
@@ -84,19 +84,26 @@ public class MatrixFormulation {
        String line = "";
        String cvsSplitBy = ",(?=([^\"]*\"[^\"]*\")*[^\"]*$)";
        //String[][] data = new String[][];
+       HashMap<String, HashMap<Integer,Object>> trainColumns = new HashMap<String,HashMap<Integer, Object>>();
        LinkedList<String[]> data= new LinkedList<String[]>(); 
        LinkedList<String[]> dataTrain = new LinkedList<String[]>();
        LinkedList<String[]> dataTest = new LinkedList<String[]>();
        HashMap<String, MatrixDataString> testTrain = new HashMap<String, MatrixDataString>();
        String[][] csvMatrixTrain =  null, csvMatrixTest = null;
-       HashMap<Integer, String> name = new HashMap<Integer, String>();
-       HashMap<Integer, String> review = new HashMap<Integer,String>();
-       HashMap<Integer, Integer> rating = new HashMap<Integer, Integer>();
-       HashMap<Integer, String> reviewClean = new HashMap<Integer, String>();
-       HashMap<Integer, Integer> sentiment = new HashMap<Integer, Integer>();
-       HashMap<Integer, HashMap<String, Integer>> wordCount = new HashMap<Integer, HashMap<String, Integer>>();
-       
-       
+//       HashMap<Integer, String> name = new HashMap<Integer, String>();
+//       HashMap<Integer, String> review = new HashMap<Integer,String>();
+//       HashMap<Integer, Integer> rating = new HashMap<Integer, Integer>();
+//       HashMap<Integer, String> reviewClean = new HashMap<Integer, String>();
+//       HashMap<Integer, Integer> sentiment = new HashMap<Integer, Integer>();
+//       HashMap<Integer, HashMap<String, Integer>> wordCount = new HashMap<Integer, HashMap<String, Integer>>();
+//       
+//       HashMap<Integer, String> nameClone = new HashMap<Integer, String>();;
+//       HashMap<Integer, String> reviewClone = new HashMap<Integer,String>();;
+//       HashMap<Integer, Integer> ratingClone = new HashMap<Integer, Integer>();;
+//       HashMap<Integer, String> reviewCleanClone = new HashMap<Integer, String>();;
+//       HashMap<Integer, Integer> sentimentClone = new HashMap<Integer, Integer>();;
+//       HashMap<Integer, HashMap<String, Integer>> wordCountClone = new HashMap<Integer, HashMap<String, Integer>>();
+//       
        
        try {
     	   //BufferedReader br = new BufferedReader("/home/ankita/coursera/ml/classification/week1/module-2-assignment-train-idx.json");  
@@ -106,8 +113,8 @@ public class MatrixFormulation {
     			
     	  //Reader reader = new InputStreamReader(MatrixFormulation.class.getResourceAsStream("/home/ankita/coursera/ml/classification/week1/module-2-assignment-train-idx.json"), "UTF-8");
                Gson gson = new GsonBuilder().create();
-               int train[] = gson.fromJson(br1, int[].class);
-               int test[] = gson.fromJson(br2, int[].class);
+               HashSet<Integer> train = gson.fromJson(br1, HashSet.class);
+               HashSet<Integer> test= gson.fromJson(br2,  HashSet.class);
     		   
     		   //System.out.println(Arrays.toString(p));
                
@@ -120,55 +127,127 @@ public class MatrixFormulation {
            //newHeadings = Arrays.copyOfRange(headings, 0, headings.length);
            newHeadings[headings.length] = "review_clean";
            newHeadings[headings.length+1] = "sentiment";
+           newHeadings[headings.length+2] = "wordCount";
+           matrixTest.setColumnHeaders(newHeadings);
+           matrixTrain.setColumnHeaders(newHeadings);
            //headings = Arrays.copyOfRange(headings, 2, headings.length);
            while ((line = br.readLine()) != null) {
 
                // use comma as separator
+        	   
                String[] features = new String[newHeadings.length];
-               name.put(i, features[0]);
+               System.arraycopy(line.split(cvsSplitBy, -1), 0, features, 0,headings.length );
+               //name.put(i, features[0]);
                if(features[1].trim() == "" )
                {
-            	   review.put(i, "N/A");
+            	   features[1]= "N/A";
+            	   
+            	   
+            	   //review.put(i, "N/A");
                }
-               else
-               {
-            	   review.put(i, features[1]);
-               }
-              
-               rating.put(i, Integer.parseInt(features[2]));
-               reviewClean.put(i, remove_punctuation(features[1]));
+//               else
+//               {
+//            	   review.put(i, features[1]);
+//               }
+//              
+              // rating.put(i, Integer.parseInt(features[2]));
+               features[headings.length] = remove_punctuation(features[1]);
+               int sentimentValue = 0;
+               //reviewClean.put(i, cleanReview);
                if(Double.parseDouble(features[2]) >=4)
         	   {
-            	   sentiment.put(i, 1);
+            	   sentimentValue = 1;
         		   
         	   }
         	   else
         	   {
-            	   sentiment.put(i, -1);
+            	   sentimentValue = -1;
         	   }
                
+               HashMap<String, Integer> wordCountMap = wordCount(features[headings.length]);
+               
+               if(train.contains(i))
+               {
+            	   matrixTrain.name.put(i, features[0]);
+            	   matrixTrain.review.put(i, features[1]);
+            	   matrixTrain.rating.put(i, Integer.parseInt(features[2]));
+            	   matrixTrain.reviewClean.put(i, features[headings.length]);
+            	   matrixTrain.sentiment.put(i, sentimentValue);
+            	   matrixTrain.wordCount.put(i, wordCountMap);	
+            	   
+            	   
+               }
+               else{
+            	   matrixTest.name.put(i, features[0]);
+            	   matrixTest.review.put(i, features[1]);
+            	   matrixTest.rating.put(i, Integer.parseInt(features[2]));
+            	   matrixTest.reviewClean.put(i, features[headings.length]);
+            	   matrixTest.sentiment.put(i, sentimentValue);
+            	   matrixTest.wordCount.put(i, wordCountMap);	
+            	   
+               }
                
                
-               //System.out.println("Country [code= " + country[4] + " , name=" + country[5] + "]");
+              
                i++;
            }
-          for (int j = 0; j < train.length; j++) {
-        	  //System.out.println(j);
-			dataTrain.add(data.get(train[j]));
-		}
-          
-          for (int j = 0; j < test.length; j++) {
-        	  //System.out.println(j);
-			dataTest.add(data.get(test[j]));
-		} 
-          csvMatrixTrain = dataTrain.toArray(new String[dataTrain.size()][]);
-          csvMatrixTest = dataTest.toArray(new String[dataTest.size()][]);
-          
-            matrixTest.setColumnHeaders(newHeadings);
-            matrixTest.setMatrix(csvMatrixTest);
+           
             
-            matrixTrain.setColumnHeaders(newHeadings);
-            matrixTrain.setMatrix(csvMatrixTrain);
+//           reviewClone.putAll(review);// = review.p
+//           nameClone.putAll(name);
+//           ratingClone.putAll(rating);
+//           reviewCleanClone.putAll(reviewClean);
+//           sentimentClone.putAll(sentiment);
+//           wordCountClone.putAll(wordCount);
+//           
+//           review.keySet().retainAll(train);
+//           name.keySet().retainAll(train);
+//           rating.keySet().retainAll(train);
+//           reviewClean.keySet().retainAll(train);
+//           sentiment.keySet().retainAll(train);
+//           wordCount.keySet().retainAll(train);
+//           
+//           reviewClone.keySet().retainAll(test);
+//           nameClone.keySet().retainAll(test);
+//           ratingClone.keySet().retainAll(test);
+//           reviewCleanClone.keySet().retainAll(test);
+//           sentimentClone.keySet().retainAll(test);
+//           wordCountClone.keySet().retainAll(test);
+//          
+//           
+//           matrixTest.setColumnHeaders(newHeadings);
+//           matrixTest.setName(nameClone);
+//           matrixTest.setRating(ratingClone);
+//           matrixTest.setReview(reviewClone);
+//           matrixTest.setReviewClean(reviewCleanClone);
+//           matrixTest.setSentiment(sentimentClone);
+//           matrixTest.setWordCount(wordCountClone);
+           
+           
+//           matrixTest.setColumnHeaders(newHeadings);
+//           matrixTrain.setName(name);
+//           matrixTrain.setRating(rating);
+//           matrixTrain.setReview(review);
+//           matrixTrain.setReviewClean(reviewClean);
+//           matrixTrain.setSentiment(sentiment);
+//           matrixTrain.setWordCount(wordCount);
+//           
+           
+          // trainColumns.put("review", review);
+           
+//          for (int j = 0; j < train.length; j++) {
+//        	  //System.out.println(j);
+//			dataTrain.add(data.get(train[j]));
+//		}
+//          
+//          for (int j = 0; j < test.length; j++) {
+//        	  //System.out.println(j);
+//			dataTest.add(data.get(test[j]));
+//		} 
+          //csvMatrixTrain = dataTrain.toArray(new String[dataTrain.size()][]);
+         // csvMatrixTest = dataTest.toArray(new String[dataTest.size()][]);
+          
+           
            
             testTrain.put("train", matrixTrain);
             testTrain.put("test", matrixTest);
@@ -191,6 +270,29 @@ public class MatrixFormulation {
 
 		
 		
+	}
+	
+	public HashMap<String, Integer> wordCount(String cleanReview)
+	{
+		HashMap<String, Integer> count = new HashMap<String, Integer>();
+		
+		String tokenized[] = cleanReview.split(" ");
+		
+		for (String string : tokenized) {
+			if(string.trim() != "")
+			{
+				if(count.containsKey(string))
+				{
+					count.put(string, count.get(string) + 1);
+				}
+				else{
+					count.put(string, 1);
+				}
+			}
+			
+		}
+		
+		return count;
 	}
 	
 	public double[] doubleTypeArray(String a[]){
